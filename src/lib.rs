cd7aeaf9faa9a360ab13911aa0152e5c0fd170e7
Todd Stellanova <tstellanova@users.noreply.github.com>
2020-05-08 16:20:38 -0700

    Adding signed payloads

--- src/lib.rs
+++ src/lib.rs
@@ -25,25 +25,50 @@ pub struct Signature {
     pub sig: String,
 }
 
+
+
 #[derive(Serialize, Deserialize, Clone, Debug)]
-#[allow(non_snake_case)]
-pub struct FileInfo {
-    pub hashes: HashMap<String, String>,
-    pub length: usize,
-}
-
-pub struct SignedPayload<T> {
-    pub signatures: Vec<Signature>,
-    pub signed: T,
-}
-
-
 pub struct TimeAttestation {
     /// eg [ 1, 2 ]
     pub nonces: Vec<u32>,
     /// eg "2017-05-18T16:23:13Z"
     pub time: String,
 }
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct SignedPayload<T> {
+    pub signatures: Vec<Signature>,
+    pub signed: T,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct VehicleManifest {
+    primary_ecu_serial: String,
+    vin: String,
+    ecu_version_manifests: HashMap<String, EcuManifest>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct EcuManifest {
+    pub ecu_serial: String,
+    pub installed_image: ImageInfo,
+    pub previous_timeserver_time: String,
+    pub timeserver_time: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+#[allow(non_snake_case)]
+pub struct ImageInfo {
+    pub fileinfo: FileInfo,
+    pub filepath: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct FileInfo {
+    pub hashes: HashMap<String, String>,
+    pub length: usize,
+}
+
 
 
 
